d0af8e5ed4d5d6f693251cdaba915d328672bf26
bridge2nowhere <bridge2nowhere@yahoo.com>
2025-06-12 17:08:55 -0700

    updated dialing and files working

--- desktop/bbs.py
+++ desktop/bbs.py
@@ -1,20 +1,27 @@
 import random
 import item
+import messages
 
 class BBS:
     def __init__(self,phone_number,board_name,has_ident):
         self.phone = phone_number
         self.bname = board_name
         self.identify = has_ident
+
+        self.file_list = []
+        self.message_list = []
+
+    def first_connect(self):
         self.file_list = item.make_file_list(random.randint(5,50))
+        self.message_list = messages.make_message_list()
+
 bbs_connnected = None
 
 bbs_list = []
-bbs_list.append(BBS(585,'Free File Farm',True))
-bbs_list.append(BBS(223,'Goose Roost',False))
-bbs_list.append(BBS(233,'Daves Castle',False))
-bbs_list.append(BBS(224,'The Crystal Palace',False))
-bbs_list.append(BBS(225,'Magic Carpet',False))
+bbs_list.append(BBS(3526544,'Free File Farm',True))
+bbs_list.append(BBS(2230412,'Goose Roost',False))
+bbs_list.append(BBS(2248216,'The Crystal Palace',False))
+bbs_list.append(BBS(2257446,'Magic Carpet',False))
 
 def lookup(pnum):
     global bbs_connected
--- desktop/main.py
+++ desktop/main.py
@@ -11,44 +11,67 @@ menu_mode = 'main'
 
 
 def handle_dialing():
+    global game_mode
+    global phone_number
+
+    phone_number = int(input('Enter phone number to dial (352-2222 should be 3522222): '))
+    while bbs.lookup(phone_number) == 'dial again':
         phone_number = int(input('Enter phone number to dial (352-2222 should be 3522222): '))
-        while bbs.lookup(phone_number) == 'dial again':
-            phone_number = int(input('Enter phone number to dial (352-2222 should be 3522222): '))
-        print('Connected to' + str(phone_number))
-        print('\n\n')
-        game_mode = 'bbs_menu'
+    print('Connected to ' + str(phone_number))
+    print('\n\n')
+    if bbs.bbs_list[bbs.bbs_connected].file_list == []:
+        bbs.bbs_list[bbs.bbs_connected].first_connect()
+    game_mode = 'bbs_menu'
 
 def handle_menu():
+    global game_mode
+    global menu_mode
+    global phone_number
+
     if menu_mode == 'main':
-            print('Welcome to ' + bbs.lookup(phone_number))
-            print('[1] Messages')
-            print('[2] Download Files')
-            if bbs.bbs_list[bbs.bbs_connected].identify:
-                print('[3] Identify Files')
-            print('[4] Node Mangement')
-            print('[5] Disconnect')
-            choice = int(input('>>>'))
-            if choice == 1:
-                menu_mode = 'messages'
-            elif choice == 2:
-                menu_mode = 'files'
-            elif choice == 3 and bbs.bbs_list[bbs.bbs_connected].identify:
-                menu_mode = 'ident'
-            elif choice == 4:
-                game_mode = 'bbs_node'
-            elif choice == 5:
-                print("\n\nDisconnected\n")
-                game_mode = 'dial_tone'
-                bbs.bbs_connected = None
-                phone_number = None
-            else:
-                print('invalid selection')
+        print('Welcome to ' + bbs.lookup(phone_number))
+        print('[1] Messages')
+        print('[2] Download Files')
+        if bbs.bbs_list[bbs.bbs_connected].identify:
+            print('[3] Identify Files')
+        print('[4] Node Mangement')
+        print('[5] Disconnect')
+        choice = int(input(']>'))
+        if choice == 1:
+            menu_mode = 'messages'
+        elif choice == 2:
+            menu_mode = 'files'
+        elif choice == 3 and bbs.bbs_list[bbs.bbs_connected].identify:
+            menu_mode = 'ident'
+        elif choice == 4:
+            game_mode = 'bbs_node'
+        elif choice == 5:
+            print("\n\nDisconnected\n")
+            game_mode = 'dial_tone'
+            bbs.bbs_connected = None
+            phone_number = None
+        else:
+            print('invalid selection')
+    elif menu_mode == 'files':
+        for index,file in enumerate(bbs.bbs_list[bbs.bbs_connected].file_list):
+            print(format(index, '04X') + ' | ' + file.id)
+        user_input = input('Choose file to download: ')
+        if user_input == '':
+            menu_mode = 'main'
+        else:
+            download = int(user_input, 16)
+            print(bbs.bbs_list[bbs.bbs_connected].file_list[download].id)
+            input()
+
 
 while True:
-    test_list = item.make_file_list(20)
-    for i in test_list:
-        print(i.id + ' ' + str(i.size) +' ' + i.description)
+
     if game_mode == 'dial_tone':
+        print('=========BBS List==========')
+        print('Free File Farm     352-6544')
+        print('Goose Roost        223-0412')
+        print('The Crystal Palace 224-8216')
+        print('TMagic Carpet      225-7446')
         handle_dialing()
 
     if game_mode == 'bbs_menu':
