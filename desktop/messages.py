38e0097d921885d93fbfc77cbe0481eb3f23687b
Woodbridge <awoodbridge@adserv.osd.wednet.edu>
2025-07-12 10:22:29 -0700

    added file import

--- desktop/messages.py
+++ desktop/messages.py
@@ -1,4 +1,9 @@
 import random
+import csv
+
+#config
+file_import = True
+
 
 def make_message_list() -> 'Message[]':
     """creates a list of messages"""
@@ -25,16 +30,31 @@ class Message:
         self.chapter = chp
         self.user = usr
 
-
-
 master_message_list = []
-master_message_list.append(Message(0,'sysop99','06/12/1991','GEN', 'Hayes Modem Firmware Needed', 'Need v3.1 firmware for 2400 baud, anyone have it?'))
-master_message_list.append(Message(0,'circuit_fiend','06/05/1991','GEN', 'EPROM Failures', '8k Blank EPROMs fail checksum right out of the pack.'))
-master_message_list.append(Message(0,'sysop99','06/12/1991','GEN', 'Hayes Modem Firmware Needed', 'Need v3.1 firmware for 2400 baud, anyone have it?'))
-master_message_list.append(Message(0,'circuit_fiend','06/05/1991','GEN', 'EPROM Failures', '8k Blank EPROMs fail checksum right out of the pack.'))
-master_message_list.append(Message(0,'sysop99','06/12/1991','GEN', 'Hayes Modem Firmware Needed', 'Need v3.1 firmware for 2400 baud, anyone have it?'))
-master_message_list.append(Message(0,'circuit_fiend','06/05/1991','GEN', 'EPROM Failures', '8k Blank EPROMs fail checksum right out of the pack.'))
-master_message_list.append(Message(0,'sysop99','06/12/1991','GEN', 'Hayes Modem Firmware Needed', 'Need v3.1 firmware for 2400 baud, anyone have it?'))
-master_message_list.append(Message(0,'circuit_fiend','06/05/1991','GEN', 'EPROM Failures', '8k Blank EPROMs fail checksum right out of the pack.'))
-master_message_list.append(Message(0,'sysop99','06/12/1991','GEN', 'Hayes Modem Firmware Needed', 'Need v3.1 firmware for 2400 baud, anyone have it?'))
-master_message_list.append(Message(0,'circuit_fiend','06/05/1991','GEN', 'EPROM Failures', '8k Blank EPROMs fail checksum right out of the pack.'))
+
+if file_import:
+    print('file import turned on')
+    message_file = input('Enter CSV File: ')
+    with open(message_file, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+
+        header = True
+        for row in reader:
+
+            if not header:
+                #print(row)
+                master_message_list.append(Message(row[0],row[1],row[2],row[3], row[4], row[5]))
+            header = False
+
+else:
+    print('file import turned off')
+    master_message_list.append(Message(0,'sysop99','06/12/1991','GEN', 'Hayes Modem Firmware Needed', 'Need v3.1 firmware for 2400 baud, anyone have it?'))
+    master_message_list.append(Message(0,'circuit_fiend','06/05/1991','GEN', 'EPROM Failures', '8k Blank EPROMs fail checksum right out of the pack.'))
+    master_message_list.append(Message(0,'sysop99','06/12/1991','GEN', 'Hayes Modem Firmware Needed', 'Need v3.1 firmware for 2400 baud, anyone have it?'))
+    master_message_list.append(Message(0,'circuit_fiend','06/05/1991','GEN', 'EPROM Failures', '8k Blank EPROMs fail checksum right out of the pack.'))
+    master_message_list.append(Message(0,'sysop99','06/12/1991','GEN', 'Hayes Modem Firmware Needed', 'Need v3.1 firmware for 2400 baud, anyone have it?'))
+    master_message_list.append(Message(0,'circuit_fiend','06/05/1991','GEN', 'EPROM Failures', '8k Blank EPROMs fail checksum right out of the pack.'))
+    master_message_list.append(Message(0,'sysop99','06/12/1991','GEN', 'Hayes Modem Firmware Needed', 'Need v3.1 firmware for 2400 baud, anyone have it?'))
+    master_message_list.append(Message(0,'circuit_fiend','06/05/1991','GEN', 'EPROM Failures', '8k Blank EPROMs fail checksum right out of the pack.'))
+    master_message_list.append(Message(0,'sysop99','06/12/1991','GEN', 'Hayes Modem Firmware Needed', 'Need v3.1 firmware for 2400 baud, anyone have it?'))
+    master_message_list.append(Message(0,'circuit_fiend','06/05/1991','GEN', 'EPROM Failures', '8k Blank EPROMs fail checksum right out of the pack.'))
